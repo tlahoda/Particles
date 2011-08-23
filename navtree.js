3c7b45fd06998f48b21eaad80d14421e8ee001d9
Thomas Lahoda <tlahoda@gmail.com>
2011-08-23 16:53:38 -0400

    particles documentation

---
+++ navtree.js
@@ -0,0 +1,345 @@
+var NAVTREE =
+[
+  [ "Particles", "index.html", [
+    [ "Class List", "annotated.html", [
+      [ "particle::Action< Particle >", "classparticle_1_1Action.html", null ],
+      [ "particle::Apply< Force, Particle >", "classparticle_1_1Apply.html", null ],
+      [ "particle::ArrayStorage< Particle >", "classparticle_1_1ArrayStorage.html", null ],
+      [ "particle::Connection< Particle >", "classparticle_1_1Connection.html", null ],
+      [ "particle::ConnectionModel", "classparticle_1_1ConnectionModel.html", null ],
+      [ "particle::Constraint< Connection >", "classparticle_1_1Constraint.html", null ],
+      [ "particle::Displace< Integrator, Particle >", "classparticle_1_1Displace.html", null ],
+      [ "particle::DisplacementConstraint< Connection >", "classparticle_1_1DisplacementConstraint.html", null ],
+      [ "particle::Engine", "classparticle_1_1Engine.html", null ],
+      [ "particle::Euler", "classparticle_1_1Euler.html", null ],
+      [ "particle::GaussSeidel", "classparticle_1_1GaussSeidel.html", null ],
+      [ "particle::Grid< Storage >", "classparticle_1_1Grid.html", null ],
+      [ "particle::Group< Storage >", "classparticle_1_1Group.html", null ],
+      [ "particle::MassAcceleration", "classparticle_1_1MassAcceleration.html", null ],
+      [ "particle::Engine::MultiReaction< Iterator >", "classparticle_1_1Engine_1_1MultiReaction.html", null ],
+      [ "particle::Particle", "classparticle_1_1Particle.html", null ],
+      [ "particle::QuadModel", "classparticle_1_1QuadModel.html", null ],
+      [ "particle::Engine::Reaction< Reactant >", "classparticle_1_1Engine_1_1Reaction.html", null ],
+      [ "particle::Renderer< Model >", "classparticle_1_1Renderer.html", null ],
+      [ "particle::ResetForce< Particle >", "classparticle_1_1ResetForce.html", null ],
+      [ "particle::RungeKutta", "classparticle_1_1RungeKutta.html", null ],
+      [ "particle::Spring", "classparticle_1_1Spring.html", null ],
+      [ "particle::StrideIterator< Container >", "classparticle_1_1StrideIterator.html", null ],
+      [ "particle::VectorStorage< Particle >", "classparticle_1_1VectorStorage.html", null ],
+      [ "particle::Verlet< Correction >", "classparticle_1_1Verlet.html", null ],
+      [ "particle::VertexModel< GlType >", "classparticle_1_1VertexModel.html", null ],
+      [ "particle::ViscuousDrag", "classparticle_1_1ViscuousDrag.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "particle::Action< Particle >", "classparticle_1_1Action.html", [
+        [ "particle::Apply< Force, Particle >", "classparticle_1_1Apply.html", null ],
+        [ "particle::Displace< Integrator, Particle >", "classparticle_1_1Displace.html", null ],
+        [ "particle::ResetForce< Particle >", "classparticle_1_1ResetForce.html", null ]
+      ] ],
+      [ "particle::ArrayStorage< Particle >", "classparticle_1_1ArrayStorage.html", null ],
+      [ "particle::Connection< Particle >", "classparticle_1_1Connection.html", null ],
+      [ "particle::ConnectionModel", "classparticle_1_1ConnectionModel.html", null ],
+      [ "particle::Constraint< Connection >", "classparticle_1_1Constraint.html", [
+        [ "particle::DisplacementConstraint< Connection >", "classparticle_1_1DisplacementConstraint.html", null ]
+      ] ],
+      [ "particle::Engine", "classparticle_1_1Engine.html", null ],
+      [ "particle::Euler", "classparticle_1_1Euler.html", null ],
+      [ "particle::GaussSeidel", "classparticle_1_1GaussSeidel.html", null ],
+      [ "particle::Grid< Storage >", "classparticle_1_1Grid.html", null ],
+      [ "particle::Group< Storage >", "classparticle_1_1Group.html", null ],
+      [ "particle::MassAcceleration", "classparticle_1_1MassAcceleration.html", null ],
+      [ "particle::Engine::MultiReaction< Iterator >", "classparticle_1_1Engine_1_1MultiReaction.html", null ],
+      [ "particle::Particle", "classparticle_1_1Particle.html", null ],
+      [ "particle::QuadModel", "classparticle_1_1QuadModel.html", null ],
+      [ "particle::Engine::Reaction< Reactant >", "classparticle_1_1Engine_1_1Reaction.html", null ],
+      [ "particle::Renderer< Model >", "classparticle_1_1Renderer.html", null ],
+      [ "particle::RungeKutta", "classparticle_1_1RungeKutta.html", null ],
+      [ "particle::Spring", "classparticle_1_1Spring.html", null ],
+      [ "particle::StrideIterator< Container >", "classparticle_1_1StrideIterator.html", null ],
+      [ "particle::VectorStorage< Particle >", "classparticle_1_1VectorStorage.html", null ],
+      [ "particle::Verlet< Correction >", "classparticle_1_1Verlet.html", null ],
+      [ "particle::VertexModel< GlType >", "classparticle_1_1VertexModel.html", null ],
+      [ "particle::ViscuousDrag", "classparticle_1_1ViscuousDrag.html", null ]
+    ] ],
+    [ "Class Members", "functions.html", null ],
+    [ "Namespace List", "namespaces.html", [
+      [ "particle", "namespaceparticle.html", null ]
+    ] ],
+    [ "Namespace Members", "namespacemembers.html", null ],
+    [ "File List", "files.html", [
+      [ "Action.h", "Action_8h.html", null ],
+      [ "Apply.h", "Apply_8h.html", null ],
+      [ "ArrayStorage.h", "ArrayStorage_8h.html", null ],
+      [ "common.h", "common_8h.html", null ],
+      [ "Connection.h", "Connection_8h.html", null ],
+      [ "ConnectionModel.h", "ConnectionModel_8h.html", null ],
+      [ "Constraint.h", "Constraint_8h.html", null ],
+      [ "Displace.h", "Displace_8h.html", null ],
+      [ "DisplacementConstraint.h", "DisplacementConstraint_8h.html", null ],
+      [ "Engine.h", "Engine_8h.html", null ],
+      [ "Euler.h", "Euler_8h.html", null ],
+      [ "GaussSeidel.h", "GaussSeidel_8h.html", null ],
+      [ "Grid.h", "Grid_8h.html", null ],
+      [ "Group.h", "Group_8h.html", null ],
+      [ "MassAcceleration.h", "MassAcceleration_8h.html", null ],
+      [ "Particle.h", "Particle_8h.html", null ],
+      [ "QuadModel.h", "QuadModel_8h.html", null ],
+      [ "Renderer.h", "Renderer_8h.html", null ],
+      [ "ResetForce.h", "ResetForce_8h.html", null ],
+      [ "RungeKutta.h", "RungeKutta_8h.html", null ],
+      [ "Spring.h", "Spring_8h.html", null ],
+      [ "StrideIterator.h", "StrideIterator_8h.html", null ],
+      [ "VectorStorage.h", "VectorStorage_8h.html", null ],
+      [ "Verlet.h", "Verlet_8h.html", null ],
+      [ "VertexModel.h", "VertexModel_8h.html", null ],
+      [ "ViscuousDrag.h", "ViscuousDrag_8h.html", null ]
+    ] ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
